0197eca7852fe8ab874c6167d48d917e02c88acc
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-02-27 17:55:02 +0200

    Зміни до дев'ятого ДЗ

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let switchInterval = null;
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+        switchInterval = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();      
+        }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(switchInterval);
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
